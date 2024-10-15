433ed04dd7cc1d46367ab1d45d096db859faefc9
Kim Dehmlow <5627401+kideh88@users.noreply.github.com>
2024-10-15 17:02:16 +0200

    [APP-3167] Add flask app based on the code from slack bot repo

---
+++ src/start-app.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+echo "Starting App"
+
+export token="$DATAROBOT_API_TOKEN"
+export endpoint="$DATAROBOT_ENDPOINT"
+
+gunicorn -b :8080 flask_app:flask_app  --timeout 200 --graceful-timeout 30 &
